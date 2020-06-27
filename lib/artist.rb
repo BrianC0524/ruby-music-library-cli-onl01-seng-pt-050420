6ae67018e473bee151efa2187f4724c5e8eb8d8a
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-27 00:56:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ class Artist
     if song.artist == nil 
       song.artist = self
     end
-    if !@songs.include?(song)
+    if @songs.include?(song) == false
       @song << song
     end
   end
