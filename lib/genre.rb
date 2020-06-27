42f925d8a5b25e56f24dfe8ddbacc8bbb4376d8e
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-27 00:33:45 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(new_artist)
+    genre = self.new(new_artist)
+    genre.save
+    genre
+  end
+  
+end
